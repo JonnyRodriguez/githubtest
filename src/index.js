1bbc7074bb392489997654b638156f893906f56b
Jonny Rodriguez <jojoroto@gmail.com>
2021-04-26 23:43:28 -0400

    initial

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 (async () => {
-  console.log(await repos("JonnyRodriguez"));
+  console.log(await content("data"));
 })();
 
-async function repos(user) {
-  let url = `https://api.github.com/users/${user}/repos`;
+async function content(path, repo = "githubdb", owner = "JonnyRodriguez") {
+  let url = `https://api.github.com/repos/${owner}/${repo}/contents/${path}`;
+  console.log(url);
   return await fetch(url).then((response) => response.json());
 }
